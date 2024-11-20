2f6014a1f5a0af69a7c2d8e9b6674e5ce0f4c861
Soseh <sosehv@gmail.com>
2024-11-20 11:39:12 -0500

    added assertEqual function which asserts two input arguments and prints the assertion results.

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟢 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🔴 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+assertEqual("Soseh", "Soseh");
+assertEqual(13,12);
