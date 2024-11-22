73be5d0f08751cce357f3d494a50d262a75c1845
Soseh <sosehv@gmail.com>
2024-11-22 15:53:08 -0500

    Implementing function without() that takes two arrays and returns a new array where the elements of array 2 are removed from the source array.

---
+++ without.js
@@ -0,0 +1,32 @@
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+//This function compares two arrays and prints a Pass or Fail message
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1,array2)) {
+    console.log(`🟢 Assertion Passed: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`🔴 Assertion Failed: [${array1}] !== [${array2}]`);
+  }
+};
+
+
+//This function returns a subset of a given array by removing unwanted elements
+const without = function(source, itemsToRemove) {
+let refinedSource = [];
+for (let i = 0; i < source.length; i++){
+  for (let j = 0; j < itemsToRemove.length; j++){
+    if (source[i] !== itemsToRemove[i]){
+      refinedSource.push(source[i]);
+    }
+  }
+  }
+};
