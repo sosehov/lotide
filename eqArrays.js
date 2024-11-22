33b2ba805f01fd0147dbd44f3721fab3257d5390
Soseh <sosehv@gmail.com>
2024-11-22 12:56:13 -0500

    Implementing eqArrays function that takes two arrays as parameters and returns true or false.

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+    return true;
+  }
+};
+//test cases
+assertEqual(eqArrays([1,2,3],[1,2,3]),true);
+assertEqual(eqArrays([1,2,],[1,2,3]),false);
+assertEqual(eqArrays([],[]),undefined);
+assertEqual(eqArrays([1,2,3],[1,2]),false);
+assertEqual(eqArrays([1],[]),false);
+assertEqual(eqArrays(["hi",2,3],["hi",2,3]),true);
