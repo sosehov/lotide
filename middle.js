e005f4223bf16d0211f0bce11c03894d6e49f761
Soseh <sosehv@gmail.com>
2024-11-25 11:44:32 -0500

    Implementing function middle and adding test assertions.

---
+++ middle.js
@@ -0,0 +1,44 @@
+//This function compares two arrays and return true or false
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+//This function prints a Pass message if the eqArrays return true or Fail message if eqArrays returns false
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1,array2)) {
+    console.log(`🟢 Assertion Passed: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`🔴 Assertion Failed: [${array1}] !== [${array2}]`);
+  }
+};
+//This function takes an array and returns the middle-most element(s)
+const middle = function(arr) {
+  let midArr = []; 
+  let lengthArr = arr.length;
+  if ( lengthArr < 3) {
+    return midArr;
+  }
+  else if (lengthArr % 2 !== 0){
+    midArr.push(arr[Math.round((lengthArr / 2)) - 1]);
+
+  }
+  else {
+    midArr.push(arr[(lengthArr/2) - 1], arr[lengthArr/2]);
+  }
+  return midArr;
+};
+
+//TEST CODE
+assertArraysEqual(middle([]),[]);
+assertArraysEqual(middle([1]),[]);
+assertArraysEqual(middle([1,2]),[]);
+assertArraysEqual(middle([1,2,3,4]),[2,3]);
+assertArraysEqual(middle([1,2,3,4,5]),[3]);
+assertArraysEqual(middle([1,2,3,4,5,6,7]),[4]);
