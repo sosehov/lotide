68fafe1ae62f6232c1ba3bbdc6db750026f44170
Soseh <sosehv@gmail.com>
2024-11-26 15:21:54 -0500

    Implementing findKeyByValue function and its test assertions.

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+//This is a custom assert function which takes in two values, compares them and prints a message
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//This function takes an object and a value and returns the first key which contains the given value
+const findKeyByValue = function(obj, val) {
+  let objectKeys = Object.keys(obj);
+  console.log(objectKeys);
+  for (let el of objectKeys) {
+    if (obj[el] === val) {
+      return el;
+    }
+  }
+};
+
+//TEST CODE
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
