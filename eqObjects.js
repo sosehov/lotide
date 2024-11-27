24c561e2f1f0d544e8eb4d4e02ba2651ef3af373
Soseh <sosehv@gmail.com>
2024-11-27 10:33:05 -0500

    Removing calls to eqObjects that aren't being used.

--- eqObjects.js
+++ eqObjects.js
@@ -43,17 +43,14 @@ const eqObjects = function(object1, object2) {
 
 const shirtObject = {color: "red", size: "medium"};
 const anotherShirtObject = {size: "medium", color: "red"};
-eqObjects(shirtObject, anotherShirtObject);
 assertEqual(eqObjects(shirtObject, anotherShirtObject),true);
 
 const longSleeveShirtObject = {size: "medium", color: "red", sleeveLength: "long"};
-eqObjects(shirtObject, longSleeveShirtObject);
 
 assertEqual(eqObjects(shirtObject, longSleeveShirtObject),false);
 
 const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
 const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-eqObjects(multiColorShirtObject, anotherMultiColorShirtObject); // => true
 
 const longSleeveMultiColorShirtObject = {
   size: "medium",
