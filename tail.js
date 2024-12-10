b3217023247f02f63e0bfc7a7ac73daf65bc9c7b
Soseh <sosehv@gmail.com>
2024-12-10 16:16:26 -0500

    Removing the condition that checks if the array length is 1 in the trail function.

--- tail.js
+++ tail.js
@@ -1,9 +1,9 @@
 // This function returns the tail of an array
 const tail = function(array) {
-  if (array.length === 0 || array.length === 1) {
+  if (array.length === 0) { 
     return [];
   }
-  return array.slice(1);
+  return array.slice(1); // this will return an empty array if there's only one element in the array
 };
 
 module.exports = tail;
