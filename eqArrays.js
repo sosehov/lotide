8726a0db05582821a00b35425054eb9be36e49e2
Soseh <sosehv@gmail.com>
2024-12-10 16:23:20 -0500

    Refactoring for modularity purposes.

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 //This function takes two arrays and returns true or false based on perfect match
 const eqArrays = function(arr1,arr2) {
   if (arr1.length !== arr2.length) {
@@ -17,11 +10,4 @@ const eqArrays = function(arr1,arr2) {
   }
   return true;
 };
-//test cases
-assertEqual(eqArrays([1,2,3],[1,2,3]),true);
-assertEqual(eqArrays([1,2,],[1,2,3]),false);
-assertEqual(eqArrays([2,3,],[1,2]),false);
-assertEqual(eqArrays([],[]),true);
-assertEqual(eqArrays([1,2,3],[1,2]),false);
-assertEqual(eqArrays([1],[]),false);
-assertEqual(eqArrays(["hi",2,3],["hi",2,3]),true);
+module.exports = eqArrays;
