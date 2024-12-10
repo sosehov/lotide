4da888920d65346ff2f4314d1d10f353ca6ffba2
Soseh <sosehv@gmail.com>
2024-12-10 16:10:31 -0500

    Refactoring for modularity purposes.

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// This function returns the tail of an array
 const tail = function(array) {
   if (array.length === 0 || array.length === 1) {
     return [];
@@ -12,15 +6,4 @@ const tail = function(array) {
   return array.slice(1);
 };
 
-const words = ["Hello", "Lighthouse", "Labs"];
-const result = tail(words); // should return ["Lighthouse", "Labs"]
-assertEqual(result.length, words.length -1);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-assertEqual(words.length, 3);
-const words1 = [];
-const result1 = tail(words1);
-assertEqual(result1.length, 0);
-const words2 = ["soseh"];
-const result2 = tail(words2);
-assertEqual(result2.length, words2.length - 1);
+module.exports = tail;
