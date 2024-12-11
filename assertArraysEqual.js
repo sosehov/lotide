80d9038e702b599f8215086570edd0179bc8a63a
Soseh <sosehv@gmail.com>
2024-12-11 10:43:25 -0500

    Refactoring for modularity purposes.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,5 @@
-const eqArrays = function(arr1,arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
+
 //This function compares two arrays and prints a Pass or Fail message
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1,array2)) {
@@ -18,6 +9,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-//test cases
-assertArraysEqual([23, 4, 0, 6], [23, 4, 6, 6]);
-assertArraysEqual([23, 4, 0, 6], [23, 4, 0, 6]);
+module.exports = assertArraysEqual;
