b532a195f7ce49cb3f37462bf5338ab39b0b3ca2
Soseh <sosehv@gmail.com>
2024-12-11 15:56:15 -0500

    Reordering test code.

--- map.js
+++ map.js
@@ -19,7 +19,6 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-const words = ["ground", "control", "to", "major", "tom"];
 //This function takes in an array and a callback function and returns a modified version of the same array
 const map = function(array, callback) {
   const results = [];
@@ -29,13 +28,12 @@ const map = function(array, callback) {
   return results;
 };
 
+// TEST CODE
+const words = ["ground", "control", "to", "major", "tom"];
 const results1 = map(words, (word) => word[0]);
 //console.log(results1);
-
 const results2 = map(words, (word) => word.length);
 const results3 = map(words, (word) => word + "ik");
-
-//TEST CODE
 assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
 assertArraysEqual(map(words, (word) => word[0]), ["g", "c", "t", "m", "t"]);
 assertArraysEqual(results2, [6, 7, 2, 5, 3]);
