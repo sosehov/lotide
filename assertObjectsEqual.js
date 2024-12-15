14abed95be37da485db581a75fba12775e8aa3a7
Soseh <sosehv@gmail.com>
2024-12-15 12:25:42 -0500

    Commented out the test code

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,16 +1,16 @@
-const _ = require('./index');
+const eqObjects = require('./eqObjects');
 
-//This is an assertion function that takes two objects, compares them using eqObjects() and prints out a Pass or Fail message
+// This is an assertion function that takes two objects, compares them using eqObjects() and prints out a Pass or Fail message
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require("util").inspect;
-  if (_.eqObjects(actual, expected)) {
+  if (eqObjects(actual, expected)) {
     console.log(`🟢 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
     console.log(`🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
-//TEST CODE
+/* TEST CODE
 const object1 = {a:1, b:2, c:3};
 const object2 = {a:1, b:2, c:3};
 assertObjectsEqual(object1, object2);
@@ -21,4 +21,4 @@ assertObjectsEqual(object4, object3);
 
 const object5 = {a: [1, 2, 3], b: 'hello'};
 const object6 = {b: 'hello', a: [1, 2, 3]};
-assertObjectsEqual(object5, object6);
+assertObjectsEqual(object5, object6); */
