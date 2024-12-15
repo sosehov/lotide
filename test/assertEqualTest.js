c2bb6881950fe2e3713ec7fc6ae1a4a4c4d8c58a
Soseh <sosehv@gmail.com>
2024-12-15 18:10:54 -0500

    deletes file

--- test/assertEqualTest.js
+++
@@ -1,8 +0,0 @@
-const assertEqual = require("../assertEqual");
-const assert = require('chai').assert;
-
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual("Soseh", "Soseh");
-assertEqual(13,12);
