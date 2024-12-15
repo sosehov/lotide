9bea746304678a30d0d9ed74c69a27206e90983e
Soseh <sosehv@gmail.com>
2024-12-15 12:24:23 -0500

    Refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,4 @@
-const _ = require('./index');
-
-//This assertion function prints a Pass message if the eqArrays returns true or Fail if eqArrays returns false
+// This assertion function prints a Pass message if the eqArrays returns true or Fail if eqArrays returns false
 const assertArraysEqual = function(array1, array2) {
   if (_.eqArrays(array1,array2)) {
     console.log(`🟢 Assertion Passed: [${array1}] === [${array2}]`);
@@ -9,6 +7,6 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-// TEST CODE
+/* TEST CODE
 assertArraysEqual([23, 4, 0, 6], [23, 4, 6, 6]);
-assertArraysEqual([23, 4, 0, 6], [23, 4, 0, 6]);
+assertArraysEqual([23, 4, 0, 6], [23, 4, 0, 6]); */
