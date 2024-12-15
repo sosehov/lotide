1d542a5d14d2d72a574d5e59f8ff6a1c20f5d72b
Soseh <sosehv@gmail.com>
2024-12-15 12:31:09 -0500

    Refactored

--- map.js
+++ map.js
@@ -1,6 +1,4 @@
-const _ = require('./index');
-
-//This function takes in an array and a callback function and returns a modified version of the same array
+// This function takes in an array and a callback function and returns a modified version of the same array
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -10,3 +8,14 @@ const map = function(array, callback) {
 };
 
 module.exports = map;
+
+/* TEST CODE
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, (word) => word[0]);
+//console.log(results1);
+const results2 = map(words, (word) => word.length);
+const results3 = map(words, (word) => word + "ik");
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
+assertArraysEqual(map(words, (word) => word[0]), ["g", "c", "t", "m", "t"]);
+assertArraysEqual(results2, [6, 7, 2, 5, 3]);
+assertArraysEqual(results3, ["groundik", "controlik", "toik", "majorik", "tomik"]); */
