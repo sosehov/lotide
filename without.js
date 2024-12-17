f6919a4fad76d80eee368dcc37558f688044cebf
Soseh <sosehv@gmail.com>
2024-12-17 18:12:56 -0500

    Fixed linting issues

--- without.js
+++ without.js
@@ -1,19 +1,19 @@
 // This function returns a subset of a given array by removing unwanted elements
 const without = function(source, itemsToRemove) {
-let refinedSource = [];
-for (let i = 0; i < source.length; i++){
-  let isFound = false;
-  for (let j = 0; j < itemsToRemove.length; j++){
-    if (source[i] === itemsToRemove[j]){
-      isFound = true;
-      break;
+  let refinedSource = [];
+  for (let i = 0; i < source.length; i++) {
+    let isFound = false;
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        isFound = true;
+        break;
+      }
+    }
+    if (!isFound) {
+      refinedSource .push(source[i]);
     }
   }
-  if (!isFound){
-  refinedSource .push(source[i]);
-}
-}
-return refinedSource;
+  return refinedSource;
 };
 
 module.exports = without;
