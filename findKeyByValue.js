348294178f53050a1e27d28148941474e63411d7
Soseh <sosehv@gmail.com>
2024-12-18 12:19:27 -0500

    Adds a check for input type and adds an explicit return undefined

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,16 @@
 // This function takes an object and a value and returns the first key which contains the given value
 const findKeyByValue = function(obj, val) {
-  let objectKeys = Object.keys(obj);
-  for (let el of objectKeys) {
-    if (obj[el] === val) {
-      return el;
+  // Check if the input is a valid object
+  if (typeof obj !== 'object' || obj === null) {
+    console.log('First argument must be an object');
+  }
+  for (let key in obj) {
+    if (obj[key] === val) {
+      return key;
     }
   }
+  // Return undefined explicitly when no match is found
+  return undefined;
 };
 
 module.exports = findKeyByValue;
