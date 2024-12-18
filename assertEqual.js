09dfefe073301f1b6b6986087361339740b6e70d
Soseh <sosehv@gmail.com>
2024-12-18 10:59:28 -0500

    Removes commented out test code

--- assertEqual.js
+++ assertEqual.js
@@ -6,9 +6,3 @@ const assertEqual = function(actual, expected) {
     console.log(`🔴 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-
-/* TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual("Soseh", "Soseh");
-assertEqual(13,12); */
