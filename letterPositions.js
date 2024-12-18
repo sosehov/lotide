6d3cd63d6c31de530a0628956ee2fd3517fa7063
Soseh <sosehv@gmail.com>
2024-12-18 12:00:58 -0500

    Adds code to check for spaces and check for invalid string input

--- letterPositions.js
+++ letterPositions.js
@@ -1,13 +1,23 @@
 // This function takes a string and returns an object with letters found and their indices
 const letterPositions = function(sentence) {
+  // Check if the input is a valid string
+  if (typeof sentence !== 'string') {
+    console.log('Input must be a string');
+  }
+  // Initialize an empty object to store letter positions
   let result = {};
+  // Iterate through the string and find positions for each letter
   for (let i = 0; i < sentence.length; i++) {
-    if (sentence[i] !== " ") {
-      if (result[sentence[i]]) {
-        result[sentence[i]].push(i);
-      } else {
-        result[sentence[i]] = [i];
-      }
+    const char = sentence[i];
+    // Skip spaces
+    if (char === ' '){
+      continue;
+    }
+    // Add the position of the character to the result object
+    if (result[char]) {
+      result[char].push(i);
+    } else {
+      result[char] = [i];
     }
   }
   return result;
