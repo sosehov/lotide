d43b8ae3040d5f8336ea5ace4e0bd0498651c66e
Soseh <sosehv@gmail.com>
2024-12-18 11:00:53 -0500

    Adds require eqArrays, replaces _.eqArrays with eqArrays and removes commented out code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,9 @@
+const eqArrays = require("./eqArrays");
 // This assertion function prints a Pass message if the eqArrays returns true or Fail if eqArrays returns false
 const assertArraysEqual = function(array1, array2) {
-  if (_.eqArrays(array1,array2)) {
+  if (eqArrays(array1,array2)) {
     console.log(`🟢 Assertion Passed: [${array1}] === [${array2}]`);
   } else {
     console.log(`🔴 Assertion Failed: [${array1}] !== [${array2}]`);
   }
 };
-
-/* TEST CODE
-assertArraysEqual([23, 4, 0, 6], [23, 4, 6, 6]);
-assertArraysEqual([23, 4, 0, 6], [23, 4, 0, 6]); */
