79660533d9158493fb6bed6e99d86a9c01f5a583
Soseh <sosehv@gmail.com>
2024-12-18 15:00:19 -0500

    fixes linting issues

--- flatten.js
+++ flatten.js
@@ -1,6 +1,7 @@
 // This function takes an array and flattens the inner arrays
 const flatten = function(arr) {
   let flatArr = [];
+  
   for (let i = 0; i < arr.length; i++) {
     if (Array.isArray(arr[i])) {
       flatArr = flatArr.concat(arr[i]);
