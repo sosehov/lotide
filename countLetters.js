a7f081ea47999222909c0c54fbea69837a088d7d
Soseh <sosehv@gmail.com>
2024-12-18 10:56:53 -0500

    removes commented out test code

--- countLetters.js
+++ countLetters.js
@@ -1,6 +1,7 @@
 // This function takes a string and returns an object with the letters found in that string and number of repeats
 const countLetters = function(sentence) {
   let result = {};
+  // Looping through each character in the sentence
   for (const letter of sentence) {
     if (letter !== " ") {
       result[letter] = (result[letter] || 0) + 1;
@@ -10,11 +11,3 @@ const countLetters = function(sentence) {
 };
 
 module.exports = countLetters;
-
-/* TEST CODE
-const result1 = countLetters("lightHouse in the house");
-assertEqual(result1["l"], 1);
-assertEqual(result1["h"], 3);
-const result2 = countLetters("LHL");
-assertEqual(result2["L"], 2);
-assertEqual(result2["H"], 1); */
