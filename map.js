5a817f5bc695f5fbeca209367404f422143a469f
Soseh <sosehv@gmail.com>
2024-12-18 15:14:37 -0500

    Adds code to checks if inputs are actual array and function

--- map.js
+++ map.js
@@ -1,5 +1,13 @@
 // This function takes in an array and a callback function and returns a modified version of the same array
 const map = function(array, callback) {
+  // Ensure array is an actual array and callback is a function type
+  if (!Array.isArray(array)) {
+    console.log('first argument must be an array');
+  }
+  if (typeof callback !== 'function') {
+    console.log('Second argument must be a function');
+  }
+
   const results = [];
   for (let item of array) {
     results.push(callback(item));
